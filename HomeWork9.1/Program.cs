84859a915976cdfdeb430157ef905c0368669669
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-03-13 12:07:41 +0300

    Summing numbers in the interval

---
+++ HomeWork9.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int InputInt(string msg)
+{
+    System.Console.Write($"Введите значение {msg} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbersInInterval(int start, int end)
+{
+    if (start > end) return (0);
+    return start + SumNumbersInInterval(start + 1, end);
+}
+
+int m = InputInt("M");
+int n = InputInt("N");
+System.Console.WriteLine(SumNumbersInInterval(m, n));
